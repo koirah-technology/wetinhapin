1837af131d3f121c6f72e219f3c34e09e8ce83c2
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-05-27 22:22:06 +0200

    setup playwright

---
+++ apps/wetinhapin-customer-portal/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default AboutPage
