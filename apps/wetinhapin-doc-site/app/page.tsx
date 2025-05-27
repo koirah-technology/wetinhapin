3c962f7819715c8a80983fe8db3eedcfa2b2684f
Samuel Koigor F. Turay <96601470+Koigor97@users.noreply.github.com>
2025-05-27 22:33:02 +0200

    add playwright

--- apps/wetinhapin-doc-site/app/page.tsx
+++ apps/wetinhapin-doc-site/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
@@ -6,6 +8,7 @@ export default function Page() {
         <div className="flex flex-col items-center justify-center gap-4">
           <h1 className="text-2xl font-bold">Doc-site</h1>
           <Button size="sm">Button</Button>
+          <Link href="/about">About</Link>
         </div>
       </div>
   )
